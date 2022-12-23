87ccc2c021d9baa15420987f9e90aa9d58867b73
loksangho <sangstar@gmail.com>
2022-12-24 10:48:54 +1100

    content rich text

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
+const { documentToHtmlString } = require("@contentful/rich-text-html-renderer");
+
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('css')
+  eleventyConfig.addFilter("renderRichTextAsHtml", (value) =>
+    documentToHtmlString(value)
+  );
 }
