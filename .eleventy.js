ede1fef61ab3fe38329133a74f83821e5f89cc60
loksangho <sangstar@gmail.com>
2022-12-24 21:22:10 +1100

    assets

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,8 @@ const { documentToHtmlString } = require("@contentful/rich-text-html-renderer");
 const trimWords = require('trim-words').default;
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('css')
+  eleventyConfig.addPassthroughCopy('css');
+  eleventyConfig.addPassthroughCopy('assets');
   eleventyConfig.addFilter("renderRichTextAsHtml", (value) =>
     documentToHtmlString(value)
   );
