d7933b7a3b11ecbb458112f23eb06b3bf96c61ba
loksangho <sangstar@gmail.com>
2022-12-24 20:11:40 +1100

    trim words

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const { documentToHtmlString } = require("@contentful/rich-text-html-renderer");
-
+const { trimWords } = require('trim-words');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('css')
   eleventyConfig.addFilter("renderRichTextAsHtml", (value) =>
     documentToHtmlString(value)
+  );
+  eleventyConfig.addFilter("trimWords", (value) =>
+    trimWords(value, 60, "...")
   );
 }
