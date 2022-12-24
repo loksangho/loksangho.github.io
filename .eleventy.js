ab90c4bf93291bc3b11135d30f45ce981f499c0c
loksangho <sangstar@gmail.com>
2022-12-24 17:58:12 +1100

    clean cache

--- .eleventy.js
+++ .eleventy.js
@@ -17,7 +17,7 @@ module.exports = function (eleventyConfig) {
   );
 
   // Copy the `img` and `css` folders to the output
-  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("css");
 
   // Add plugins
