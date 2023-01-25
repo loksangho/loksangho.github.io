a35a502ca580218324948b00d531e7934f566707
loksangho <sangstar@gmail.com>
2023-01-26 08:47:10 +1100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { documentToHtmlString } = require("@contentful/rich-text-html-renderer");
-const { documentToPlainTextString } = require("@contentful/rich-text-html-renderer");
+const { documentToPlainTextString } = require("@contentful/rich-text-plain-text-renderer");
 const trimWords = require('trim-words').default;
 
 module.exports = function (eleventyConfig) {
