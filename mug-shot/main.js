89b54720f42d3bfaf29e012cc027148066f32503
Lok Sang Ho <sangstar@gmail.com>
2025-07-17 11:54:56 +1000

    Update main.js

--- mug-shot/main.js
+++ mug-shot/main.js
@@ -88,7 +88,9 @@ async function init() {
 
     // Setup Scene for MediaPipe phase
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera = new THREE.Camera();
+
+    //camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 2;
     const canvas = document.getElementById('outputCanvas');
     renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true });
