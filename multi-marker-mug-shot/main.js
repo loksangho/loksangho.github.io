7f73d57597b83e98cb4940ab7fabb8a3a142ee1a
Lok Sang Ho <sangstar@gmail.com>
2025-07-19 01:08:25 +1000

    Update main.js

--- multi-marker-mug-shot/main.js
+++ multi-marker-mug-shot/main.js
@@ -307,8 +307,7 @@ async function initCombinedPlayer(profileData) {
         videoEl.style.position = 'fixed';
         videoEl.style.top = '0';
         videoEl.style.left = '0';
-        videoEl.style.width = '100vw';
-        videoEl.style.height = '100vh';
+        
         videoEl.style.objectFit = 'cover';
         videoEl.style.zIndex = '-1'; // Behind your canvas
 
