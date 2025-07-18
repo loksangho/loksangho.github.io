f209fae9104e17c996f667e8096b6a89fabc5c51
Lok Sang Ho <sangstar@gmail.com>
2025-07-18 19:26:27 +1000

    Update main.js

--- multi-marker-mug-shot/main.js
+++ multi-marker-mug-shot/main.js
@@ -281,7 +281,7 @@ async function initCombinedPlayer(profileData) {
             // 💡 FIX: The ArMultiMarkerControls constructor is flawed. We will call it
             // and then immediately and manually fix its internal parameters to ensure
             // the sub-marker list is correctly configured.
-            multiMarkerControls = new THREEx.ArMultiMarkerControls(arToolkitContext, markerRoot, {});
+            multiMarkerControls = new THREEx.ArMultiMarkerControls(arToolkitContext, scene, markerRoot, subMarkersControls);
             multiMarkerControls.parameters.subMarkersControls = subMarkersControls;
 
             console.log("Step 2: Created base ArMultiMarkerControls and manually corrected parameters.");
