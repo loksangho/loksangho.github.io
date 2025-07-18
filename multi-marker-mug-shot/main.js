d767032accdb86c656ac7c2a60789ac25c00ef82
Lok Sang Ho <sangstar@gmail.com>
2025-07-18 23:22:19 +1000

    Update main.js

--- multi-marker-mug-shot/main.js
+++ multi-marker-mug-shot/main.js
@@ -40,7 +40,7 @@ const _settings = {
     trackingFactors: [0.5, 0.4, 1.5]
   },
 
-  NNPath: '../../../neuralNets/NN_COFFEE_2.json',
+  NNPath: '../../../neuralNets/NN_COFFEE_0.json',
 
   cameraFov: 0, // In degrees, camera vertical FoV. 0 -> auto mode
   scanSettings:{
