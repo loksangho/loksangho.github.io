d907c571114ebc5a27d86fd7a5eb42eab27ae359
Lok Sang Ho <sangstar@gmail.com>
2026-05-18 09:19:32 +1000

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -7,9 +7,25 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('.well-known');
   eleventyConfig.addPassthroughCopy('css');
   eleventyConfig.addPassthroughCopy('assets');
-  eleventyConfig.addFilter("renderRichTextAsHtml", (value) =>
-    documentToHtmlString(value)
-  );
+  //eleventyConfig.addFilter("renderRichTextAsHtml", (value) =>
+  //  documentToHtmlString(value)
+  //);
+  eleventyConfig.addFilter("renderRichTextAsHtml", function(content) {
+    if (!content) return "";
+
+    const options = {
+      renderNode: {
+        // This targets embedded images/assets in your rich text
+        [BLOCKS.EMBEDDED_ASSET]: (node) => {
+          const { file, title } = node.data.target.fields;
+          // Return the actual HTML string for the image
+          return `<img src="${file.url}" alt="${title}" loading="lazy" class="rich-text-image" />`;
+        }
+      }
+    };
+
+    return documentToHtmlString(content, options);
+  });
   eleventyConfig.addFilter("renderRichTextAsPlainText", (value) =>
     documentToPlainTextString(value)
   );
